eebca9a8b179e0f152437328ba082e732ae29ce5
wangbo <781278657@qq.com>
2023-04-07 10:06:05 +0800

    perf: 🚀 优化优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import * as path from 'path'
+import { resolve } from 'path'
 import { cwd } from 'process'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -20,7 +20,7 @@ export default defineConfig(({ command, mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src')
+        '@': resolve(__dirname, 'src')
       }
     },
     server: {
