28f84b65427130a3c4c4a336df843933519e6f71
wangbo <781278657@qq.com>
2023-04-12 10:09:59 +0800

    refactor: 💡 迁移代码

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,4 @@
   <RouterView />
 </template>
 
-<script setup lang="ts">
-import { topList, addTop, login, switchSeat } from '@/utils/axios-http/example'
-
-topList({ pageNo: 1, pageSize: 10 }).then(res => {
-  console.log(res, 'topList')
-})
-
-addTop({
-  name: '王力宏',
-  org_name: '一战队',
-  achievement: '10000'
-}).then(res => {
-  console.log(res, 'addTop')
-})
-
-login({
-  userName: '',
-  passWord: '',
-  terminaltype: 'WEB',
-  auto: 0
-}).then(res => {
-  console.log(res, 'login')
-})
-
-switchSeat({
-  cdId: '31017',
-  companyId: '9',
-  currentId: '1624058289024442368',
-  newId: '1624058289024442368',
-  profileId: '67098'
-}).then(res => {
-  console.log(res, 'switch')
-})
-</script>
+<script setup lang="ts"></script>
