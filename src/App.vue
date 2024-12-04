6616441b2939f692c19d4ccae53d95d5b038da52
wangbo <781278657@qq.com>
2024-12-04 10:58:28 +0800

    build: 🔧 Add unplugin-auto-import for auto imports

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+const msg = ref('You did it!')
 </script>
 
 <template>
@@ -8,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld :msg="msg" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,22 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), UnoCSS()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    UnoCSS(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: './auto-imports.d.ts',
+      dumpUnimportItems: true,
+      vueTemplate: true
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
